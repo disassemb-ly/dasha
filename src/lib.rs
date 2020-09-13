4106dab0ff0b8a7ad9b203ee8c7188eb6ba9cbfc
William Tange <will@brt.sexy>
2020-09-13 18:51:34 +0200

    preliminary impl of add inst

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,23 @@ pub struct Dasha;
 
 impl Dasha {
     pub fn disasm<'a, I: IntoIterator<Item = &'a u8>>(i: I) -> Result<Vec<Inst>, Error> {
-        let _i = i.into_iter();
+        let mut i = i.into_iter();
+        let mut insts = vec![];
 
-        let insts = vec![];
+        while let Some(op) = i.next() {
+            insts.push(match op {
+                0x00 => match i.next() {
+                    Some(modrm) if modrm.mod_bits() == 0b11 => {
+                        Inst::Add(Addr::Direct(modrm.reg()), Addr::Direct(modrm.rm()))
+                    }
+                    Some(_) => unimplemented!(), // FIXME(s1g)
+                    #[allow(unreachable_patterns)]
+                    Some(_) => unreachable!(),
+                    None => return Err(Error::ExpectedModRm),
+                },
+                _ => unimplemented!(),
+            });
+        }
 
         Ok(insts)
     }
