579fccf156a3d765d36f0aa22af5a62bbd0dc9ae
William Tange <will@brt.sexy>
2020-09-13 18:51:34 +0200

    preliminary impl of add inst

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,16 @@
 use std::{error, fmt};
 
 #[derive(Debug, PartialEq)]
-pub enum Error {}
+pub enum Error {
+    ExpectedModRm,
+}
 
 impl error::Error for Error {}
 
 impl fmt::Display for Error {
-    fn fmt(&self, _f: &mut fmt::Formatter) -> fmt::Result {
-        unimplemented!()
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::ExpectedModRm => write!(f, "expected mod-reg-r/m byte"),
+        }
     }
 }
