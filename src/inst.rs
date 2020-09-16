5760bce02ac8f2596c190700d49adf00abf2e707
William Tange <will@brt.sexy>
2020-09-16 18:55:11 +0200

    impl or through cmp insts (with macro)

--- src/inst.rs
+++ src/inst.rs
@@ -5,18 +5,39 @@ use crate::{Addr, DisplayFormat, Format};
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Inst {
     Add(Addr, Addr),
+    Or(Addr, Addr),
+    Adc(Addr, Addr),
+    Sbb(Addr, Addr),
+    And(Addr, Addr),
+    Sub(Addr, Addr),
+    Xor(Addr, Addr),
+    Cmp(Addr, Addr),
 }
 
 impl DisplayFormat for Inst {
     fn fmt(&self, fmt: Format, f: &mut fmt::Formatter) -> fmt::Result {
+        macro_rules! write_inst {
+            ( $inst:expr, $op1:expr, $op2:expr ) => {
+                write!(
+                    f,
+                    "{}{} {}, {}",
+                    $inst,
+                    $op1.size().assert_eq($op2.size()).display(fmt),
+                    $op1.display(fmt),
+                    $op2.display(fmt)
+                )
+            };
+        }
+
         match (self, fmt) {
-            (Inst::Add(op1, op2), Format::Att) => write!(
-                f,
-                "add{} {}, {}",
-                op1.size().assert_eq(op2.size()).display(fmt),
-                op1.display(fmt),
-                op2.display(fmt)
-            ),
+            (Inst::Add(op1, op2), Format::Att) => write_inst!("add", op1, op2),
+            (Inst::Or(op1, op2), Format::Att) => write_inst!("or", op1, op2),
+            (Inst::Adc(op1, op2), Format::Att) => write_inst!("adc", op1, op2),
+            (Inst::Sbb(op1, op2), Format::Att) => write_inst!("sbb", op1, op2),
+            (Inst::And(op1, op2), Format::Att) => write_inst!("and", op1, op2),
+            (Inst::Sub(op1, op2), Format::Att) => write_inst!("sub", op1, op2),
+            (Inst::Xor(op1, op2), Format::Att) => write_inst!("xor", op1, op2),
+            (Inst::Cmp(op1, op2), Format::Att) => write_inst!("cmp", op1, op2),
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,133 @@ pub use reg::Reg;
 
 pub struct Dasha;
 
+macro_rules! read_inst {
+    ( $inst:path, $iter:expr ) => {
+        match $iter.next() {
+            Some(modrm) if modrm.mod_bits() == 0b00 && modrm.rm(Size::Long) == Reg::Esp => {
+                match $iter.next() {
+                    Some(sib)
+                        if sib.index(Size::Long) == Reg::Esp
+                            && sib.base(Size::Long) == Reg::Esp =>
+                    {
+                        $inst(
+                            Addr::Direct(modrm.reg(Size::Byte)),
+                            Addr::Indirect(Indirect::Base(Size::Byte, Reg::Esp)),
+                        )
+                    }
+                    Some(sib)
+                        if sib.index(Size::Long) == Reg::Esp
+                            && sib.base(Size::Long) == Reg::Ebp =>
+                    {
+                        $inst(
+                            Addr::Direct(modrm.reg(Size::Byte)),
+                            Addr::Indirect(Indirect::OffsetIndexScale(
+                                Size::Byte,
+                                Offset::I32($iter.read_le().ok_or(Error::ExpectedOffsetLong)?),
+                                Reg::Eiz,
+                                sib.scale(),
+                            )),
+                        )
+                    }
+                    Some(sib) if sib.index(Size::Long) == Reg::Esp => $inst(
+                        Addr::Direct(modrm.reg(Size::Byte)),
+                        Addr::Indirect(Indirect::BaseIndexScale(
+                            Size::Byte,
+                            sib.base(Size::Long),
+                            Reg::Eiz,
+                            sib.scale(),
+                        )),
+                    ),
+                    Some(sib) if sib.base(Size::Long) == Reg::Ebp => $inst(
+                        Addr::Direct(modrm.reg(Size::Byte)),
+                        Addr::Indirect(Indirect::OffsetIndexScale(
+                            Size::Byte,
+                            Offset::I32($iter.read_le().ok_or(Error::ExpectedOffsetLong)?),
+                            sib.index(Size::Long),
+                            sib.scale(),
+                        )),
+                    ),
+                    Some(sib) => $inst(
+                        Addr::Direct(modrm.reg(Size::Byte)),
+                        Addr::Indirect(Indirect::BaseIndexScale(
+                            Size::Byte,
+                            sib.base(Size::Long),
+                            sib.index(Size::Long),
+                            sib.scale(),
+                        )),
+                    ),
+                    None => return Err(Error::ExpectedSib),
+                }
+            }
+            Some(modrm) if modrm.mod_bits() == 0b00 && modrm.rm(Size::Long) == Reg::Ebp => $inst(
+                Addr::Direct(modrm.reg(Size::Byte)),
+                Addr::Indirect(Indirect::Mem(
+                    Size::Byte,
+                    Offset::I32($iter.read_le().ok_or(Error::ExpectedOffsetLong)?),
+                )),
+            ),
+            Some(modrm) if modrm.mod_bits() == 0b00 => $inst(
+                Addr::Direct(modrm.reg(Size::Byte)),
+                Addr::Indirect(Indirect::Base(Size::Byte, modrm.rm(Size::Long))),
+            ),
+            Some(modrm) if modrm.mod_bits() == 0b01 && modrm.rm(Size::Long) == Reg::Esp => {
+                match $iter.next() {
+                    Some(sib) => $inst(
+                        Addr::Direct(modrm.reg(Size::Byte)),
+                        Addr::Indirect(Indirect::OffsetBaseIndexScale(
+                            Size::Byte,
+                            Offset::I8($iter.read_le().ok_or(Error::ExpectedOffsetByte)?),
+                            sib.base(Size::Long),
+                            sib.index(Size::Long),
+                            sib.scale(),
+                        )),
+                    ),
+                    None => return Err(Error::ExpectedSib),
+                }
+            }
+            Some(modrm) if modrm.mod_bits() == 0b01 => $inst(
+                Addr::Direct(modrm.reg(Size::Byte)),
+                Addr::Indirect(Indirect::OffsetBase(
+                    Size::Byte,
+                    Offset::I8($iter.read_le().ok_or(Error::ExpectedOffsetByte)?),
+                    modrm.rm(Size::Long),
+                )),
+            ),
+            Some(modrm) if modrm.mod_bits() == 0b10 && modrm.rm(Size::Long) == Reg::Esp => {
+                match $iter.next() {
+                    Some(sib) => $inst(
+                        Addr::Direct(modrm.reg(Size::Byte)),
+                        Addr::Indirect(Indirect::OffsetBaseIndexScale(
+                            Size::Byte,
+                            Offset::I32($iter.read_le().ok_or(Error::ExpectedOffsetLong)?),
+                            sib.base(Size::Long),
+                            sib.index(Size::Long),
+                            sib.scale(),
+                        )),
+                    ),
+                    None => return Err(Error::ExpectedSib),
+                }
+            }
+            Some(modrm) if modrm.mod_bits() == 0b10 => $inst(
+                Addr::Direct(modrm.reg(Size::Byte)),
+                Addr::Indirect(Indirect::OffsetBase(
+                    Size::Byte,
+                    Offset::I32($iter.read_le().ok_or(Error::ExpectedOffsetLong)?),
+                    modrm.rm(Size::Long),
+                )),
+            ),
+            Some(modrm) if modrm.mod_bits() == 0b11 => $inst(
+                Addr::Direct(modrm.reg(Size::Byte)),
+                Addr::Direct(modrm.rm(Size::Byte)),
+            ),
+            Some(_) => unimplemented!(), // FIXME(s1g)
+            #[allow(unreachable_patterns)]
+            Some(_) => unreachable!(),
+            None => return Err(Error::ExpectedModRm),
+        }
+    };
+}
+
 impl Dasha {
     pub fn disasm<'a, I: IntoIterator<Item = &'a u8>>(i: I) -> Result<Vec<Inst>, Error> {
         let mut i = i.into_iter();
@@ -23,130 +150,14 @@ impl Dasha {
 
         while let Some(op) = i.next() {
             insts.push(match op {
-                0x00 => match i.next() {
-                    Some(modrm) if modrm.mod_bits() == 0b00 && modrm.rm(Size::Long) == Reg::Esp => {
-                        match i.next() {
-                            Some(sib)
-                                if sib.index(Size::Long) == Reg::Esp
-                                    && sib.base(Size::Long) == Reg::Esp =>
-                            {
-                                Inst::Add(
-                                    Addr::Direct(modrm.reg(Size::Byte)),
-                                    Addr::Indirect(Indirect::Base(Size::Byte, Reg::Esp)),
-                                )
-                            }
-                            Some(sib)
-                                if sib.index(Size::Long) == Reg::Esp
-                                    && sib.base(Size::Long) == Reg::Ebp =>
-                            {
-                                Inst::Add(
-                                    Addr::Direct(modrm.reg(Size::Byte)),
-                                    Addr::Indirect(Indirect::OffsetIndexScale(
-                                        Size::Byte,
-                                        Offset::I32(i.read_le().ok_or(Error::ExpectedOffsetLong)?),
-                                        Reg::Eiz,
-                                        sib.scale(),
-                                    )),
-                                )
-                            }
-                            Some(sib) if sib.index(Size::Long) == Reg::Esp => Inst::Add(
-                                Addr::Direct(modrm.reg(Size::Byte)),
-                                Addr::Indirect(Indirect::BaseIndexScale(
-                                    Size::Byte,
-                                    sib.base(Size::Long),
-                                    Reg::Eiz,
-                                    sib.scale(),
-                                )),
-                            ),
-                            Some(sib) if sib.base(Size::Long) == Reg::Ebp => Inst::Add(
-                                Addr::Direct(modrm.reg(Size::Byte)),
-                                Addr::Indirect(Indirect::OffsetIndexScale(
-                                    Size::Byte,
-                                    Offset::I32(i.read_le().ok_or(Error::ExpectedOffsetLong)?),
-                                    sib.index(Size::Long),
-                                    sib.scale(),
-                                )),
-                            ),
-                            Some(sib) => Inst::Add(
-                                Addr::Direct(modrm.reg(Size::Byte)),
-                                Addr::Indirect(Indirect::BaseIndexScale(
-                                    Size::Byte,
-                                    sib.base(Size::Long),
-                                    sib.index(Size::Long),
-                                    sib.scale(),
-                                )),
-                            ),
-                            None => return Err(Error::ExpectedSib),
-                        }
-                    }
-                    Some(modrm) if modrm.mod_bits() == 0b00 && modrm.rm(Size::Long) == Reg::Ebp => {
-                        Inst::Add(
-                            Addr::Direct(modrm.reg(Size::Byte)),
-                            Addr::Indirect(Indirect::Mem(
-                                Size::Byte,
-                                Offset::I32(i.read_le().ok_or(Error::ExpectedOffsetLong)?),
-                            )),
-                        )
-                    }
-                    Some(modrm) if modrm.mod_bits() == 0b00 => Inst::Add(
-                        Addr::Direct(modrm.reg(Size::Byte)),
-                        Addr::Indirect(Indirect::Base(Size::Byte, modrm.rm(Size::Long))),
-                    ),
-                    Some(modrm) if modrm.mod_bits() == 0b01 && modrm.rm(Size::Long) == Reg::Esp => {
-                        match i.next() {
-                            Some(sib) => Inst::Add(
-                                Addr::Direct(modrm.reg(Size::Byte)),
-                                Addr::Indirect(Indirect::OffsetBaseIndexScale(
-                                    Size::Byte,
-                                    Offset::I8(i.read_le().ok_or(Error::ExpectedOffsetByte)?),
-                                    sib.base(Size::Long),
-                                    sib.index(Size::Long),
-                                    sib.scale(),
-                                )),
-                            ),
-                            None => return Err(Error::ExpectedSib),
-                        }
-                    }
-                    Some(modrm) if modrm.mod_bits() == 0b01 => Inst::Add(
-                        Addr::Direct(modrm.reg(Size::Byte)),
-                        Addr::Indirect(Indirect::OffsetBase(
-                            Size::Byte,
-                            Offset::I8(i.read_le().ok_or(Error::ExpectedOffsetByte)?),
-                            modrm.rm(Size::Long),
-                        )),
-                    ),
-                    Some(modrm) if modrm.mod_bits() == 0b10 && modrm.rm(Size::Long) == Reg::Esp => {
-                        match i.next() {
-                            Some(sib) => Inst::Add(
-                                Addr::Direct(modrm.reg(Size::Byte)),
-                                Addr::Indirect(Indirect::OffsetBaseIndexScale(
-                                    Size::Byte,
-                                    Offset::I32(i.read_le().ok_or(Error::ExpectedOffsetLong)?),
-                                    sib.base(Size::Long),
-                                    sib.index(Size::Long),
-                                    sib.scale(),
-                                )),
-                            ),
-                            None => return Err(Error::ExpectedSib),
-                        }
-                    }
-                    Some(modrm) if modrm.mod_bits() == 0b10 => Inst::Add(
-                        Addr::Direct(modrm.reg(Size::Byte)),
-                        Addr::Indirect(Indirect::OffsetBase(
-                            Size::Byte,
-                            Offset::I32(i.read_le().ok_or(Error::ExpectedOffsetLong)?),
-                            modrm.rm(Size::Long),
-                        )),
-                    ),
-                    Some(modrm) if modrm.mod_bits() == 0b11 => Inst::Add(
-                        Addr::Direct(modrm.reg(Size::Byte)),
-                        Addr::Direct(modrm.rm(Size::Byte)),
-                    ),
-                    Some(_) => unimplemented!(), // FIXME(s1g)
-                    #[allow(unreachable_patterns)]
-                    Some(_) => unreachable!(),
-                    None => return Err(Error::ExpectedModRm),
-                },
+                0x00 => read_inst!(Inst::Add, &mut i),
+                0x08 => read_inst!(Inst::Or, &mut i),
+                0x10 => read_inst!(Inst::Adc, &mut i),
+                0x18 => read_inst!(Inst::Sbb, &mut i),
+                0x20 => read_inst!(Inst::And, &mut i),
+                0x28 => read_inst!(Inst::Sub, &mut i),
+                0x30 => read_inst!(Inst::Xor, &mut i),
+                0x38 => read_inst!(Inst::Cmp, &mut i),
                 _ => unimplemented!(),
             });
         }
